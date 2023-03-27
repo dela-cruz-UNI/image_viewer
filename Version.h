a570c8e2a0891e8d4eaafe28fbd5d1e11e1e062c
dela-cruz-UNI <joshua.dela@stud.unifi.it>
2023-03-27 18:22:00 +0200

    Added support for bmp files.

--- Version.h
+++ Version.h
@@ -5,6 +5,6 @@
 #ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
 #define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
 
-static const std::string version = "v1.0";
+static const std::string version = "v2.0";
 
 #endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
