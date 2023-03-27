8a044fd1d1f38deabcfe31a12e7982615b3d4a36
dela-cruz-UNI <joshua.dela@stud.unifi.it>
2023-03-27 18:30:34 +0200

    Added support for bmp files.

--- BitmapImage.cpp
+++ BitmapImage.cpp
@@ -7,12 +7,20 @@ BitmapImage::BitmapImage() : width(640), height(480), channels(3), depth(8) {
 bool BitmapImage::load(std::string name) {
     // check it is a supported image file
     std::string fileExt = name.substr(name.find_last_of(".") + 1);
-    if ((fileExt == "jpg") || (fileExt == "png") || (fileExt == "bmp")) {
+    if ((fileExt == "jpg") || (fileExt == "png") || (fileExt == "bmp")) || (fileExt == "jpeg") {
         // load image file
         // decompress into buffer
         return true;
     }
     return false;
+}
+
+bool BitmapImage::resize(int width, int height){
+    if(width <= 0 || height <= 0) return false;
+    this->width = width;
+    this->height = height;
+    //resize buffer
+    return true;
 }
 
 BitmapImage::~BitmapImage() {
