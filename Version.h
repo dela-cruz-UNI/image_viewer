896b703c4d0d37321c6f0dff7095e375bce4ffff
dela-cruz-UNI <joshua.dela@stud.unifi.it>
2023-03-27 17:28:18 +0200

    Added version name

---
+++ Version.h
@@ -0,0 +1,10 @@
+//
+// Created by dela on 27/03/23.
+//
+
+#ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+#define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+
+static const std::string version = "v1.0";
+
+#endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
